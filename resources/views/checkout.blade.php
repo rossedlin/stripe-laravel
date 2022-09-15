c6c479deecc4db67cf936ed05d0b85e16ae4f418
Ross Edlin <rossedlin@gmail.com>
2022-09-15 14:07:35 +0100

    Change button names.

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -27,13 +27,13 @@
         <div class="col-2">
             <form action="/test" method="POST">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <button type="submit" id="checkout-test-button" class="btn btn-primary">Test Mode</button>
+                <button type="submit" id="checkout-test-button" class="btn btn-primary">Checkout (Test)</button>
             </form>
         </div>
         <div class="col-2">
             <form action="/live" method="POST">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <button type="submit" id="checkout-live-button" class="btn btn-success">LIVE Mode</button>
+                <button type="submit" id="checkout-live-button" class="btn btn-success">Checkout (LIVE)</button>
             </form>
         </div>
         <div class="col-4"></div>
